2d532ee64b22d776b0c778ef641b624a69f9e605
“Odedina <odedina.adewunmi@outlook.com>
2022-07-07 23:18:36 +0100

    setState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
+/* eslint-disable no-useless-constructor */
+import { Component } from 'react';
+
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      name: {lastName: "Odedina", firstName: "Ade"},
+      country: "Nigeria"
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hello, My name is {this.state.name.firstName} {this.state.name.lastName}  and I am from {this.state.country}.
+          </p>
+         <button onClick={()=> {
+          this.setState({name: {lastName: "Adewunmi", firstName: "Joshua"}})
+         }}>
+         Change name
+         </button>
+        </header>
+      </div>
+    );
+  }
+ 
 }
 
 export default App;
